e6ed588da7854cbacc3a8064b9df8ec5e4d73451
Park Chan Jun <dpldp93@gmail.com>
2020-11-19 16:51:31 +0900

    조명 색 바뀌는 스크립트 작성

---
+++ Assets/Script/InteriolLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteriolLight : MonoBehaviour
+{
+    public Color startColor;
+    public Color endColor;
+    bool isTurnOn = true;
+    float interpolate = 0.0f; // 0.0 ~ 1.f
+
+    void Start()
+    {
+        GetComponent<Light>().color = startColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTurnOn)
+        {
+            interpolate += Time.deltaTime * 0.5f;
+            if (interpolate > 1.0f)
+                isTurnOn = false;
+        }
+        else
+        {
+            interpolate -= Time.deltaTime * 0.5f;
+            if (interpolate < 0.0f)
+                isTurnOn = true;
+        }
+
+        GetComponent<Light>().color = Color.Lerp(startColor, endColor, interpolate);
+    }
+}
