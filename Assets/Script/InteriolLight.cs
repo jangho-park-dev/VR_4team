04697c93a815732753890fab212a349d0665676f
Park Chan Jun <dpldp93@gmail.com>
2020-11-26 15:17:45 +0900

    merge전 커밋

--- Assets/Script/InteriolLight.cs
+++ Assets/Script/InteriolLight.cs
@@ -8,9 +8,11 @@ public class InteriolLight : MonoBehaviour
     public Color endColor;
     bool isTurnOn = true;
     float interpolate = 0.0f; // 0.0 ~ 1.f
+    float randomSpeed;
 
     void Start()
     {
+        randomSpeed = Random.Range(0.3f, 1.5f);
         GetComponent<Light>().color = startColor;
     }
 
@@ -19,14 +21,14 @@ public class InteriolLight : MonoBehaviour
     {
         if (isTurnOn)
         {
-            interpolate += Time.deltaTime * 0.5f;
-            if (interpolate > 1.0f)
+            interpolate += Time.deltaTime * randomSpeed;
+            if (interpolate > 1)
                 isTurnOn = false;
         }
         else
         {
-            interpolate -= Time.deltaTime * 0.5f;
-            if (interpolate < 0.0f)
+            interpolate -= Time.deltaTime * randomSpeed;
+            if (interpolate < 0)
                 isTurnOn = true;
         }
 
