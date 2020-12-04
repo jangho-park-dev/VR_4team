09bb6e4f75229141578ca81a3fb0e9c170f3405c
ouoaz <35379289p@gmail.com>
2020-12-04 20:21:21 +0900

    스코어 완료

--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -7,7 +7,6 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public Text scoreText;
-    private int score = 0;
 
     void Awake()
     {
@@ -23,12 +22,12 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //AddScore(30);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetScore();
     }
 }
