b9557bebc41851a59d83ebc39a461266df3b0b97
ouoaz <35379289p@gmail.com>
2020-12-04 19:19:50 +0900

    스코어 UI

---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public Text scoreText;
+    private int score = 0;
+
+    void Awake()
+    {
+        if (!instance)
+            instance = this;
+    }
+
+    public void AddScore(int num)
+    {
+        score += num;
+        scoreText.text = "Score : " + score;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AddScore(30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
